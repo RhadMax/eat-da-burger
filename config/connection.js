5b7e6e7b3db455ef9bb5eb408dd053bb7957b6e9
Max Patten <amanthus2535@gmail.com>
2019-07-05 16:54:58 -0700

    added jawsDB connection block and also minor cosmetic tweaks

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
+  port: process.env.PORT || 3306,
   user: "root",
   password: process.env.PASS,
   database: "burgersDB"

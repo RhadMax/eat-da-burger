41fea607b30a2661c844c2358efa1524d61663f1
Max Patten <amanthus2535@gmail.com>
2019-07-05 15:08:29 -0700

    updated burger.js model to fit revised ORM for assignment

--- models/burger.js
+++ models/burger.js
@@ -3,23 +3,19 @@ var orm = require("../config/orm.js");
 
 var burger = {
   all: function(cb) {
-    orm.all("burgers", function(res) {
+    orm.selectAll("burgers", function(res) {
       cb(res);
     });
   },
   // The variables cols and vals are arrays.
-  create: function(cols, vals, cb) {
-    orm.create("burgers", cols, vals, function(res) {
+  create: function(column, value, cb) {
+    orm.insertOne("burgers", column, value, function(res) {
       cb(res);
     });
   },
-  update: function(objColVals, condition, cb) {
-    orm.update("burgers", objColVals, condition, function(res) {
-      cb(res);
-    });
-  },
-  delete: function(condition, cb) {
-    orm.delete("burgers", condition, function(res) {
+  update: function(condition, id, cb) {
+    condition = "devoured = true"
+    orm.updateOne("burgers", condition, id, function(res) {
       cb(res);
     });
   }

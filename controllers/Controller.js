d54864734b871fed8aa3d5ecbd7415ef700e2e8b
Max Patten <amanthus2535@gmail.com>
2019-07-05 14:43:17 -0700

    reworked handlebars to fit rough format shape

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -6,35 +6,32 @@ var router = express.Router();
 var cat = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
-  cat.all(function(data) {
+router.get("/", function (req, res) {
+  cat.all(function (data) {
     var hbsObject = {
-      cats: data
+      burgers: data
     };
     console.log(hbsObject);
     res.render("index", hbsObject);
   });
 });
 
-router.post("/api/cats", function(req, res) {
-  cat.create([
-    "name", "sleepy"
-  ], [
-    req.body.name, req.body.sleepy
-  ], function(result) {
-    // Send back the ID of the new quote
-    res.json({ id: result.insertId });
-  });
+router.post("/api/cats", function (req, res) {
+  cat.create(["name"],
+    [req.body.name], function (result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    });
 });
 
-router.put("/api/cats/:id", function(req, res) {
+router.put("/api/cats/:id", function (req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
   cat.update({
     sleepy: req.body.sleepy
-  }, condition, function(result) {
+  }, condition, function (result) {
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
@@ -44,10 +41,10 @@ router.put("/api/cats/:id", function(req, res) {
   });
 });
 
-router.delete("/api/cats/:id", function(req, res) {
+router.delete("/api/cats/:id", function (req, res) {
   var condition = "id = " + req.params.id;
 
-  cat.delete(condition, function(result) {
+  cat.delete(condition, function (result) {
     if (result.affectedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
